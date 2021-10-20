23a9b5c810b6a7edab2330982c0e65f20bdf0ff6
yamato090 <yamauchi09012@gmail.com>
2021-10-20 09:33:01 +0000

    add 6章【権限・バリデーションの設定】

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :upda]
+  resources :users, only: [:show, :edit, :update]
   
 end
